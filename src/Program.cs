339a304698d071a9c5e870b0015a3ee35379e38e
devINVISIBLE <devinvisible@gmail.com>
2018-01-31 23:24:15 -0500

    Create the config.json file the first time if it doesnt exist

--- src/Program.cs
+++ src/Program.cs
@@ -5,8 +5,10 @@ using Discord.WebSocket;
 using DiscordBot.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DiscordBot
@@ -65,10 +67,25 @@ namespace DiscordBot
 
         private IConfiguration BuildConfig()
         {
-            Console.WriteLine($"Reading configuration from: {Directory.GetCurrentDirectory()}\\config.json");
+            var directory = Path.GetDirectoryName(typeof(BotConfig).GetTypeInfo().Assembly.Location);
+            var filepath = Path.Combine(directory, "config.json");
+
+            if (!File.Exists(filepath))
+            {
+                var defaultConfig = new
+                {
+                    token = "YOUR TOKEN HERE",
+                    prefix = "?"
+                };
+
+                File.WriteAllText(filepath, JsonConvert.SerializeObject(defaultConfig));
+                throw new FileNotFoundException($"Config file not found. Generating a template at '{filepath}'. Put your bot token in here.");
+            }
+
+            Console.WriteLine($"Reading configuration from: {filepath}");
             return new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("config.json")
+                .AddJsonFile(filepath)
                 .Build();
         }
     }
